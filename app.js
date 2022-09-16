4b37f902ced707e97b034656174fae8fc6b58b8b
JohnnRen <john.ren.contact@gmail.com>
2022-09-17 00:25:14 +0800

    Update

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-function Sound(src,volume=0.5,replay=false,loop=false,autoplay=false) {
+Number.prototype.round = function (n) {
+  const d = Math.pow(10, n);
+  return Math.floor((this + Number.EPSILON) * d) / d;
+};
+function Sound(src, volume = 0.5, replay = false, loop = false, autoplay = false) {
   this.sound = document.createElement("audio");
-  this.sound.volume=volume;
-  this.sound.style.display="none"
+  this.sound.volume = volume;
+  this.sound.style.display = "none";
   this.sound.src = src;
   this.sound.setAttribute("preload", "auto");
   this.sound.setAttribute("controls", "none");
-  loop&&  this.sound.setAttribute("loop", loop);
-    autoplay&&  this.sound.setAttribute("autoplay", loop);
+  loop && this.sound.setAttribute("loop", loop);
+  autoplay && this.sound.setAttribute("autoplay", loop);
 
   //this.sound.style.display = "none";
   document.body.appendChild(this.sound);
-  this.play = function() {
-    replay?this.sound.currentTime=0:false;
+  this.play = function () {
+    replay ? this.sound.currentTime = 0 : false;
     this.sound.play();
-  }
-  this.stop = function() {
+  };
+  this.stop = function () {
     this.sound.pause();
-    this.sound.currentTime = 0
-  }
+    this.sound.currentTime = 0;
+  };
 }
-var audioBgm = new Sound('bgm.mp3',0.5,false,true,true);
-var audioClick = new Sound('bubble.mp3',0.8,true);
-var audioM = new Sound('meow.mp3',0.2);
-var audioSweet = new Sound('meow2.mp3',0.2);
+var audioBgm = new Sound('bgm.mp3', 0.5, false, true, true);
+var audioClick = new Sound('bubble.mp3', 0.8, true);
+var audioM = new Sound('meow.mp3', 0.2);
+var audioSweet = new Sound('meow2.mp3', 0.2);
 var audioUnlock = new Sound('unlock.wav');
 var audioCheer = new Sound('cheer.wav');
-const bgmStart=false;
+var bgmStart = false;
 var study = new Vue({
   el: "#vue-app",
   data: {
-    target: 1000,
+    target: 10000000000,
     count: 1,
     addition: 1,
     bonus: 1,
@@ -42,60 +46,87 @@ var study = new Vue({
     helpers: HELPERS
   },
   methods: {
-    click: function() {
-     !bgmStart&& setTimeout(()=>{
-            audioBgm.play()
-bgmStart=true
-      },50)
-
-            audioClick.play()
-      this.count += this.addition * this.bonus
+    restart: function () {
+      localStorage.clear();
+      window.location.reload();
     },
-    fullScreen:function(){
-             document.documentElement.requestFullscreen()
+    click: function () {
+      !bgmStart && setTimeout(() => {
+        audioBgm.play();
+        bgmStart = true;
+      }, 50);
+
+      audioClick.play();
+      this.count += this.addition * this.bonus;
+    },
+    fullScreen: function () {
+      document.documentElement.requestFullscreen();
 
     },
-    buyUpgrade: function(upgrade) {
-      upgrade.id%2==0?audioM.play():audioSweet.play()
-      if (this.count < upgrade.price) { return }
+    buyUpgrade: function (upgrade) {
+      upgrade.id % 2 == 0 ? audioM.play() : audioSweet.play();
+      if (this.count < upgrade.price) { return; }
       // if (upgrade.level >= upgrade.id*30) { return }
       this.count -= upgrade.price;
-      this.addition += upgrade.addition;
-      // upgrade.price=(upgrade.price*1.1 ).toFixed()
+      this.addition += upgrade.base;
       upgrade.level++;
+      upgrade.base *= upgrade.growth;
+      if (upgrade.base < 10) {
+        upgrade.base = upgrade.base.round(1);
+      } else {
+        upgrade.base = upgrade.base.round(0);
+      }
+      upgrade.price *= upgrade.growth ** 2;
+      if (upgrade.price < 10) {
+        upgrade.price = upgrade.price.round(1);
+      } else {
+        upgrade.price = upgrade.price.round(0);
+      }
+      // upgrade.price=(upgrade.price*1.1 ).round()
     },
-    buyEquipment: function(e) {
-      audioUnlock.play()
-      if (this.count < e.price) { return }
+    buyEquipment: function (equipment) {
+      audioUnlock.play();
+      if (this.count < equipment.price) { return; }
       // if (upgrade.level >= upgrade.id*30) { return }
-      this.count -= e.price;
-      this.bonus += e.addition;
+      this.count -= equipment.price;
+      this.bonus += equipment.base;
 
-      // upgrade.price=(upgrade.price*1.1 ).toFixed()
-      e.bought = true;
+      // upgrade.price=(upgrade.price*1.1 ).round()
+      equipment.bought = true;
     },
-    buyHelper: function(e) {
-            audioCheer.play()
-      if (this.count < e.price) { return }
+    buyHelper: function (helper) {
+      audioCheer.play();
+      if (this.count < helper.price) { return; }
       // if (upgrade.level >= upgrade.id*30) { return }
-      this.count -= e.price;
-      this.auto += e.addition;
-    
-      // upgrade.price=(upgrade.price*1.1 ).toFixed()
-      e.bought = true;
+      this.count -= helper.price;
+      this.auto += helper.base;
+
+      // upgrade.price=(upgrade.price*1.1 ).round()
+      helper.bought = true;
     },
   },
   computed: {
-    countD: function() {
-      return Math.round(this.count)
+    countD: function () {
+      return Math.round(this.count);
     },
   },
-  mounted: function() {
-    setInterval(() => {
-      if(this.count>this.target){
-        this.target*=100
+  mounted: function () {
+    let save = localStorage.getItem("save");
+    if (save) {
+      console.log(save);
+      Object.assign(this.$data, JSON.parse(save));
+      if (this.count >= 10) {
+        this.showUpgrade = true;
       }
-      this.count += this.addition * this.bonus / 4 * this.auto
+      if (this.count >= 250) { this.showEquipment = true; }
+      if (this.count >= 500) { this.showHelper = true; }
+    }
+
+    setInterval(() => {
+      // if (this.count > this.target) {
+      // this.target *= 100;
+      // }
+      this.count += this.addition * this.bonus / 4 * this.auto;
     }, 250);
   },
   watch: {
@@ -103,12 +134,27 @@ bgmStart=true
     {
       handler(v, o) {
         if (v >= 10) {
-          this.showUpgrade = true
+          this.showUpgrade = true;
         }
-        if (v >= 700) { this.showEquipment = true }
-        if (v >= 4000) {this.showHelper=true}
+        if (v >= 250) { this.showEquipment = true; }
+        if (v >= 500) { this.showHelper = true; }
+        let save = {
+          target: this.target,
+          count: this.count,
+          addition: this.addition,
+          bonus: this.bonus,
+          auto: this.auto,
+          showUpgrade: this.showUpgrade,
+          showEquipment: this.showEquipment,
+          showHelper: this.showHelper,
+          upgrades: this.upgrades,
+          equipments: this.equipments,
+          helpers: this.helpers,
+        };
+        console.log(save);
+        localStorage.setItem("save", JSON.stringify(save));
       },
-      immediate: true
+      immediate: false
     }
   }
 
